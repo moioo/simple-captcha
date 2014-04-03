6f6ce56ac96ad5a84ef31381dbde5c603b8997c5
moioo <moioo@qq.com>
2014-04-03 20:16:51 +0800

    fix migration generator

--- lib/generators/simple_captcha_generator.rb
+++ lib/generators/simple_captcha_generator.rb
@@ -2,20 +2,23 @@ require 'rails/generators'
 
 class SimpleCaptchaGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
-                          
-  def self.source_root
-    @source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates/'))
-  end
+  source_root File.expand_path('../templates', __FILE__)
+  desc "Copy  migration to your application."
+  class_option :orm, :type => :string, :aliases => "-o", :default => "active_record", :desc => "ORM options: active_record"
 
-  def self.next_migration_number(dirname)
-    Time.now.strftime("%Y%m%d%H%M%S")
-  end
-  
-  def create_partial
+  def copy_files
+    orm = options[:orm].to_s
+    orm = "active_record" unless %w{active_record mongoid}.include?(orm)
+    if orm == "active_record"
+      migration_template "migration.rb", "db/migrate/create_simple_captcha_data.rb"
+    end
     template "partial.erb", File.join('app/views', 'simple_captcha', "_simple_captcha.erb")
   end
-  
-  def create_migration
-    migration_template "migration.rb", File.join('db/migrate', "create_simple_captcha_data.rb")
+  def self.next_migration_number(dirname)
+   if ActiveRecord::Base.timestamped_migrations
+     Time.now.utc.strftime("%Y%m%d%H%M%S")
+   else
+     "%.3d" % (current_migration_number(dirname) + 1)
+   end
   end
 end
